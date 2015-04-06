32de00aad5f7af0989672ad657b48b3974c57b6d
John Lee <jy.johnlee@gmail.com>
2015-04-05 22:44:19 -0700

    wunderlist iOS initial commit

---
+++ Wunderlist-iOS/Wunderlist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wunderlist
+//
+//  Created by John Lee on 4/5/15.
+//  Copyright (c) 2015 johnjlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
